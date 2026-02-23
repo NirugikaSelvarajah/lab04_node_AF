bcee3217e87a63fc68ce34e6cd53242ffe3215c5
NirugikaSelvarajah <snirugikagmail.com>
2026-02-23 21:27:00 +0530

    Create web server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-cat > app.js <<'EOF'
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-EOF
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+console.log("Server running at http://localhost:8080");
